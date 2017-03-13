189dbeb0fa0eb5fc06887c76ff4340fd87a64bfd
OctoCath <catherine.chu.tingyi@gmail.com>
2017-03-13 13:59:22 -0700

    Update water.c
    Was stuck for a while on water.c;
    Husby tried to help by submitting his codes into this folder, but I couldn't understand some of what his code is doing (it seemed like more advanced commands not taught in CS50...)
    I was also stubborn in wanting to find a way based on what CS50 already showed me;
    So I tried to straight out my thought process/logic again - this time I tried water.c with if-else :) - now unstuck!
    
    Would love to learn other/better ways of implementing this! (Using the content in CS50.)

--- water.c
+++ water.c
@@ -1,17 +1,22 @@
-#include <cs50.h>
-#include <stdio.h>
-
     //shower spits out 1.5 gal/min 
     //1 gal = 128 ounces, and so that shower spits out 1.5 × 128 = 192 ounces of water per minute. 
     //A typical bottle of water (that you might have for a drink, not a shower), meanwhile, might be 16 ounces. 
     //So taking a 1-minute shower is akin to using 192 ÷ 16 = 12 bottles of water. 
     //a 10-minute shower is like using 120 bottles of water.
 
+#include <cs50.h>
+#include <stdio.h>
+
 int main(void)
 {
     printf("Minutes: ");
     int x = get_int();
-//add a conditional statement so that input must be positive (n>0)
-
-    printf("Bottles: %d\n", x*12);
+    if (x>=1)
+    {
+        printf("Bottles: %d\n", x*12);
+    }
+    else
+    {
+        printf("Try again with a positive integer larger than 0.\n");
+    }
 }
