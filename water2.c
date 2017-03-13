1c7d87c770885b01eb6a286c448392dccfd81fa3
OctoCath <catherine.chu.tingyi@gmail.com>
2017-03-13 13:13:15 -0700

    Update water2.c
    Moved assignment water.c context comments on top;
    Changed else printf line to "Try again with a positive integer larger than 0."
    Statement will show up when 0 is the input.

--- water2.c
+++ water2.c
@@ -1,23 +1,22 @@
-#include <cs50.h>
-#include <stdio.h>
-
     //shower spits out 1.5 gal/min 
     //1 gal = 128 ounces, and so that shower spits out 1.5 × 128 = 192 ounces of water per minute. 
     //A typical bottle of water (that you might have for a drink, not a shower), meanwhile, might be 16 ounces. 
     //So taking a 1-minute shower is akin to using 192 ÷ 16 = 12 bottles of water. 
     //a 10-minute shower is like using 120 bottles of water.
 
+#include <cs50.h>
+#include <stdio.h>
+
 int main(void)
 {
     printf("Minutes: ");
     int x = get_int();
-    
     if (x>=1)
     {
         printf("Bottles: %d\n", x*12);
     }
     else
     {
-        printf("Retry: Enter a positive integer larger than 0 for results in Bottles\n");
+        printf("Try again with a positive integer larger than 0.\n");
     }
 }
