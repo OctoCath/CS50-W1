6739f015ed86b7c03603dfd8e1e9a0626572cd71
HawxChen <yingjheng.chen@gmail.com>
2017-03-13 01:36:53 -0700

    use unsigned int and overflow detection

--- water_hawx.c
+++ water_hawx.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
+#include<limits.h>
 int main(void) {
-    int x;
+    unsigned int x;
     do {
         printf("Minutes: ");
-        scanf("%d", &x);
-    } while(x < 0);
+        scanf("%u", &x);
+
+        if(x <= UINT_MAX/12) break;
+
+        puts("It will be overflow!!!");
+    } while(1);
     printf("Bottles: %d\n", x*12);
 }
 
