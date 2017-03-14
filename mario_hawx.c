19d2e246518e308ed8edb289a361baa7777e7079
HawxChen <yingjheng.chen@gmail.com>
2017-03-14 11:08:20 -0700

    I did it because this in an interview question of one famous company

---
+++ mario_hawx.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void) {
+    int d;
+    printf("Height: ");
+    do {
+        scanf("%d", &d);
+    } while(d <= 0 && d > 23);
+    if(d == 1) puts("##");
+
+    int len = d+1;
+
+    for(int i = 0; i < d; i++) {
+        for(int idx = 0; idx < len;idx++) {
+//            printf("%d, %d, %d; ", idx, len, len-2);
+            if(idx >= len -i - 2) printf("#");
+            else printf(" ");
+        }
+        puts("");
+    }
+
+}
