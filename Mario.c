3d8b2c839957c620a5c0c2574a836735fa595b76
OctoCath <catherine.chu.tingyi@gmail.com>
2017-03-21 22:09:32 -0700

    Update Mario.c

--- Mario.c
+++ Mario.c
@@ -1,6 +1,30 @@
-#include <stdio.h>
 #include <cs50.h>
-
-int main(void) {
-
- 
+#include <stdio.h>
+int main()
+{
+    int h;
+    int acrsum; 
+    do {
+        printf("Height: ");
+        h = get_int();
+    } while(h < 0 || h > 23);
+    
+    if(h==1)
+    {
+        printf("##\n");
+    }
+    else
+    {
+        acrsum = h + 1;
+        
+        for(int across=0; across < h; across++)
+        {
+            for(int down=0; down < acrsum; down++)
+            {
+            if(down >= acrsum-across - 2) printf("#"); 
+            else printf(" ");
+            }
+            printf("\n");
+        }
+    }
+}
